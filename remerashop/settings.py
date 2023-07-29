78cb3dae9303dd311166f5810d736cafc8af7df8
Leonardo Bortolini <128438900+Beginner1979@users.noreply.github.com>
2023-07-29 09:40:01 +0000

    mejora settings.py

--- remerashop/settings.py
+++ remerashop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2*n19eb=(#@-+=ve9=dllg#5o%13^10zgt=$4(fdjyj-*+k7!*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Leonardo79.pythonanywhere.com"]
 
 # Application definition
 
